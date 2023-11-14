d24c472d59cf57744724f5febf2cc08b66693926
Even-tr <tronstad.even@gmail.com>
2023-11-14 19:13:49 +0100

    removed possible bug

--- pipeline.py
+++ pipeline.py
@@ -45,7 +45,7 @@ class CustomTransformer(BaseEstimator, TransformerMixin):
         _BMI(self, X, y=None, threshold=30): Imputes Obesity from BMI for missing obseity values
         _fix_polydipsia(self, df, threshold=2.5): Imputes Polydipsia from Urination for missing polydipsia values
         transform(self, X, y=None, threshold_BMI=30, threshold_Polydipsia= 2.5): Using the forward function to set X 
-        set_output(self, *, transform: Literal['default', 'pandas'] | None = None) -> BaseEstimator: Set output to pandas dataframe
+        set_output(self, *, transform) -> BaseEstimator: Set output to pandas dataframe
 
     """
 
@@ -92,7 +92,7 @@ class CustomTransformer(BaseEstimator, TransformerMixin):
         return X
    
     
-    def set_output(self, *, transform: Literal['default', 'pandas'] | None = None) -> BaseEstimator:
+    def set_output(self, *, transform) -> BaseEstimator:
         return super().set_output(transform=transform)
 
 
